d6a4dab58cae8739de064330befaabc3167ac45f
spenser-jony <ddf>
2023-09-07 14:13:17 +0200

    Initial Commit

---
+++ MySet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySet
+//
+//  Created by Ivan Bezsonov on 07.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
